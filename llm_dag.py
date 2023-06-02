6a70dc4ad2508b685293d670b572c86c1d87bdae
giuliofilippi <giuliofilippi@me.com>
2023-06-02 16:52:09 +0100

    google/flan-t5-base

--- llm_dag.py
+++ llm_dag.py
@@ -1,15 +1,27 @@
-# graph tools import
+# Graph tools import
 from graph_tools import *
 
-# environment variables
+# Environment variables
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-# langchain imports
-from langchain.llms import OpenAI
-llm = OpenAI(model_name='davinci')
+# Huggingface imports
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+model_name = 'google/flan-t5-base'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-# query
-llm('hello')
+# Langchain imports
+
+# generate text from model
+def generate(model,text):
+    tokens = tokenizer(text, return_tensors="pt")
+    tokens_out = model.generate(**tokens, max_new_tokens=100)
+    text_out = str(tokenizer.batch_decode(tokens_out, skip_special_tokens=True))
+    return text_out
+
+# test
+line = 'what is your name?'
+print(generate(model=model,text=line))
 
 
