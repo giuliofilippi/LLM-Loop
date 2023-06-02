16739c3621d9d30f3027b1a254e41b2c434df6c0
giuliofilippi <giuliofilippi@me.com>
2023-06-02 11:39:36 +0100

    first commit

---
+++ graph_tools.py
@@ -0,0 +1,35 @@
+# imports
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# function to draw the graph
+def draw_graph(graph):
+    '''
+    graph: graph as dictionnary
+    graph_layout: networkx layout
+    '''
+    # Create a directed graph from the adjacency dictionary
+    G = nx.DiGraph(graph)
+
+    # Create a plot
+    pos = nx.spring_layout(G)  # Layout algorithm for graph visualization
+    nx.draw_networkx(G, pos, with_labels=True, node_size=500, node_color='lightblue',
+                    arrowstyle='->', arrowsize=10, font_size=12, font_color='black')
+
+    # Show the plot
+    plt.show()
+
+# function to get neighbours of a vertex
+def neighbours(v, graph):
+    '''
+    graph: graph as dictionnary
+    '''
+    return list(graph[v])
+
+# function to get the degree of a vertex
+def degree(v, graph):
+    '''
+    graph: graph as dictionnary
+    '''
+    return len(neighbours(v, graph))
+
